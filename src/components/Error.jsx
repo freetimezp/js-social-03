4a457332ecfd51364cea19f7d895cb099b0283b3
freetimezp <kraftukrnet@ukr.net>
2022-10-08 12:28:15 +0300

    04. js-social-03. Loading, error components.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Error = () => (
-  <div>ErrorLoader</div>
-);
+const Error = () => {
+    return (
+        <div className="w-full flex items-center justify-center flex-col">
+            <h3 className="mt-5 text-red-300">Oops, ERROR</h3>
+            <h1 className="font-bold text-2xl text-white mt-2">
+                Something wrong.. please, try later..
+            </h1>
+        </div>
+    );
+}
 
 export default Error;
