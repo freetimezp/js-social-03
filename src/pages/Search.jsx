434fd9b0dbb35967644bc603b2f76b61ef742642
freetimezp <kraftukrnet@ukr.net>
2022-10-09 18:39:05 +0300

    16. js-social-03. SearchBar component.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,11 @@
-const Search = () => (
-  <div>Search</div>
-);
+import React from "react";
+
+const Search = () => {
+    return (
+        <div>
+            Search
+        </div>
+    );
+}
 
 export default Search;
